6417bcd29dcae2e6357dd8937f03622fd9d8bdc4
Piotr Maciejewski <piotr.maciejewski@olx.pl>
2018-04-23 14:41:19 +0200

    merge

--- merge.sh
+++ merge.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-timestamp() {
-  date +"%T"
-}
+timestamp=$(date +%Y%m%d_%H%M%S)
 
 git branch
+#echo ${timestamp}
 git checkout production
-git merge origin master
-git tag timestamp
+git pull production
+git merge -X theirs origin/master
+git tag ${timestamp}
+git push --tags
 
